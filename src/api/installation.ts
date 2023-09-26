cf8714607f6a58954ea29dfd9aa8da632eecc156
Adham Farrag <adham.farrag@hotmail.com>
2023-09-26 12:36:13 +0800

    feat: mostUsed

--- src/api/installation.ts
+++ src/api/installation.ts
@@ -2,7 +2,7 @@ import { PackageManagerName } from '../types';
 import { isLinux } from '../utils/process';
 import { consola } from 'consola';
 
-export const installGlobally = async (pm: PackageManagerName, packages: string[]) => {
+export const installGlobally = async (pm: PackageManagerName, packages: string[], mostUsed?: boolean) => {
     if (isLinux) {
         consola.info('Installing globally on Linux is not supported yet.');
     } else {
