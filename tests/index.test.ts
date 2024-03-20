de7c4faf6371274453bf042c4ab924a94d19cae5
Adham Farrag <adham.farrag@hotmail.com>
2024-03-21 03:41:35 +0800

    feat: migrate from Vitest to Bun

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,7 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect } from 'bun:test';
 
 describe('Whatever', () => {
-    it('should work', () => {
-        expect(1).toBe(1);
-    });
+  it('should work', () => {
+    expect(1).toBe(1);
+  });
 })
